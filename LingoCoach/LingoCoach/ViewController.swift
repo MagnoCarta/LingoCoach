622cdbee299083f1f05dbf20668f8f5e97ebc3f2
Gilberto Magno <gilbertomagno@MacBook-Pro-de-Gilberto.local>
2020-11-18 09:16:53 -0300

    First commit

---
+++ LingoCoach/LingoCoach/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LingoCoach
+//
+//  Created by Gilberto Magno on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
