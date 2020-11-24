4f11893109d43acd3d72b543fc5f2d0001ba4ee7
vBryann <vitorbryan_melo@hotmail.com>
2020-11-24 09:05:22 -0300

    Details create

---
+++ LingoCoach/Controller/DetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.swift
+//  LingoCoach
+//
+//  Created by Vitor Bryan on 24/11/20.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
