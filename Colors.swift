a09bfe137ab32a145ca3c7de7a59195dbf430d52
lucasagosti <lucasagostif@gmail.com>
2020-11-24 10:34:08 -0300

    adicionando arquivo Colors

---
+++ Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  LingoCoach
+//
+//  Created by Lucas Fernandes on 24/11/20.
+//
+
+import UIKit
+
+extension UIColor {
+    static let background = #colorLiteral(red: 0.9607843137, green: 0.9529411765, blue: 0.9568627451, alpha: 1)
+    static let textBlack = #colorLiteral(red: 0.1176470588, green: 0.1176470588, blue: 0.1176470588, alpha: 1)
+    static let textWhite = #colorLiteral(red: 0.9607843137, green: 0.9529411765, blue: 0.9568627451, alpha: 1)
+    static let brightGreenLC = #colorLiteral(red: 0.1843137255, green: 0.7607843137, blue: 0.6470588235, alpha: 1)
+    static let darkGreenLC = #colorLiteral(red: 0.09411764706, green: 0.3882352941, blue: 0.3764705882, alpha: 1)
+}
