7057880b0901704fed93e25f4e3cf51548d4f169
vBryann <vitorbryan_melo@hotmail.com>
2020-11-24 10:16:51 -0300

    Details started

--- LingoCoach/Controller/DetailsViewController.swift
+++ LingoCoach/Controller/DetailsViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    fileprivate let botView: UIView = {
+        let botView = UIView()
+        botView.translatesAutoresizingMaskIntoConstraints = false
+        return botView
+    }()
     
+    fileprivate let topView: UIView = {
+        let topView = UIView()
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        return topView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.addSubview(botView)
+        view.addSubview(topView)
+        
     }
 
 }
