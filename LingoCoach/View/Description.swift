703bac9a98bc832f93dbf2745c3cb09f88173f4e
vBryann <vitorbryan_melo@hotmail.com>
2020-11-26 08:27:25 -0300

    Set Constraints

--- LingoCoach/View/Description.swift
+++ LingoCoach/View/Description.swift
@@ -21,7 +21,7 @@ class Description: UIView {
     let iconView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .cyan
+        view.backgroundColor = #colorLiteral(red: 0, green: 0.3953939676, blue: 0.378708303, alpha: 1)
         view.layer.cornerRadius = 8
         return view
     }()
@@ -30,7 +30,15 @@ class Description: UIView {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.layer.cornerRadius = 12
-//        view.backgroundColor = 
+        view.backgroundColor = #colorLiteral(red: 0.909702003, green: 0.9098611474, blue: 0.9097036719, alpha: 1)
+        return view
+    }() 
+    
+    let notesView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 12
+        view.backgroundColor = #colorLiteral(red: 0.9111861587, green: 0.9112922549, blue: 0.9111371636, alpha: 1)
         return view
     }()
     
@@ -42,7 +50,7 @@ class Description: UIView {
         button.layer.cornerRadius = 16
         button.setImage(icon, for: .normal)
         button.imageView?.contentMode = .scaleAspectFit
-//        button.backgroundColor =
+        button.backgroundColor = #colorLiteral(red: 0, green: 0.7753016353, blue: 0.6421442628, alpha: 1)
 //        action
         return button
         
@@ -53,10 +61,10 @@ class Description: UIView {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.layer.cornerRadius = 8
         button.clipsToBounds = true
-        button.setTitle("Teste", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.setTitle("Teste Label", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         button.setTitleColor(UIColor.white, for: .normal)
-//        button.backgroundColor =
+        button.backgroundColor = #colorLiteral(red: 0.001636183239, green: 0.7755811214, blue: 0.6421516538, alpha: 1)
         return button
         
     }()
@@ -91,8 +99,10 @@ class Description: UIView {
         backgroundColor = .white
         addSubview(iconView)
         addSubview(icon)
-//        addSubview(addCategory)
-//        addSubview(category)
+        addSubview(descriptionView)
+        addSubview(notesView)
+        addSubview(category)
+        addSubview(addCategory)
 //        addSubview(favorite)
 //        addSubview(language)
         
@@ -106,6 +116,23 @@ class Description: UIView {
 //                                     icon.trailingAnchor.constraint(equalTo: iconView.trailingAnchor),
                                      icon.widthAnchor.constraint(equalTo: iconView.widthAnchor, multiplier: 0.8),
                                      icon.heightAnchor.constraint(equalTo: iconView.heightAnchor, multiplier: 0.8)])
+        
+        NSLayoutConstraint.activate([descriptionView.topAnchor.constraint(equalTo: iconView.bottomAnchor, constant: 26),
+                                     descriptionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+                                     descriptionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+                                     descriptionView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.2)])
+        
+        NSLayoutConstraint.activate([notesView.topAnchor.constraint(equalTo: descriptionView.bottomAnchor, constant: 18),
+                                     notesView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+                                     notesView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+                                     notesView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5)])
+        
+        NSLayoutConstraint.activate([category.topAnchor.constraint(equalTo: descriptionView.topAnchor, constant: 10),
+                                     category.leadingAnchor.constraint(equalTo: descriptionView.leadingAnchor, constant: 16),
+                                     category.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3)])
+        
+        NSLayoutConstraint.activate([addCategory.topAnchor.constraint(equalTo: descriptionView.topAnchor, constant: 10),
+                                     addCategory.leadingAnchor.constraint(equalTo: category.trailingAnchor, constant: 5)])
         
     }
     
