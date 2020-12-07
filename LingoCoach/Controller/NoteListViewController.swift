9fad23f41ddfd50e6d8e6b4475465ca2fb01676f
Rudigus <rodrigo.matos9@hotmail.com>
2020-12-07 13:10:23 -0300

    now category is implemented

--- LingoCoach/Controller/NoteListViewController.swift
+++ LingoCoach/Controller/NoteListViewController.swift
@@ -28,6 +28,7 @@ class NoteListViewController: UIViewController {
         self.title = "Notas"
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationController?.navigationBar.barTintColor = .navColor
+//        self.navigationController?.navigationBar.isTranslucent = false
         self.navigationItem.rightBarButtonItem = plusButton
         navigationController?.navigationBar.tintColor = .brightBlueNL
         self.view.backgroundColor = .background
@@ -52,7 +53,7 @@ class NoteListViewController: UIViewController {
         let context = UIApplication.shared.context
         let noteToAdd = Note(context: context.self)
         noteToAdd.icon = UIImage(named: "translate")
-        noteToAdd.language = "Idioma"
+        noteToAdd.language = ""
         noteToAdd.title = "Nota \(notes.count+1)"
         do {
             try context.save()
